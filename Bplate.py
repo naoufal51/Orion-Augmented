db25ab7c2809c30e711ce109f558e0bdd07b2606
naoufal51 <naoufal51@gmail.com>
2018-01-04 19:01:21 +0100

    new

--- Bplate.py
+++ Bplate.py
@@ -8,33 +8,6 @@ Corrected csi matrix for one sub-carrier.
 
 """
 
-#
-# def final_matrix(csi_corr, csi_target):
-#
-#     csi_cr = csi_matrix(csi_corr)
-#     csi_tr = csi_matrix(csi_target)
-#
-#     rl_dod = np.zeros(3)
-#     rl_doa = np.zeros(3)
-#
-#     for i in range(0, 3):
-#         rl_dod[i] = np.angle(csi_cr[0, i, 0, 0] * np.conjugate(csi_cr[1, i, 0, 0]))
-#     for i in range(1, 3):
-#         rl_doa[i] = np.angle(csi_cr[0, 0, 0, 0] * np.conjugate(csi_cr[0, i, 0, 0]))
-#     csi_tro = np.zeros((2, 3, 1, np.size(csi_tr, 3)), dtype=complex)
-#     for k in range(0, np.size(csi_tr, 3) - 1):
-#
-#         csi_tro[0, :, 0, k] = csi_tr[0, :, 0, k]
-#
-#         for i in range(0, 3):
-#             csi_tro[1, i, 0, k] = csi_tr[1, i, 0, k] * np.exp(1j * rl_dod[i])
-#
-#         for i in range(1, 3):
-#             csi_tro[:, i, 0, k] = csi_tro[:, i, 0, k] * np.exp(1j * rl_doa[i])
-#     return csi_tro
-
-
-
 """csi_matrix, reads the csi matrix , removes the spatial mapping,
 and stores it in a 'processing friendly' array.
 
@@ -47,10 +20,35 @@ and stores it in a 'processing friendly' array.
    """
 
 
-class csi_matrix:
-    def __init__(self):
-        pass
 
+
+class csi_matrix:
+
+    def __init__(self, csi_corr, csi_target):
+             return self.final_matrix(csi_corr, csi_target)
+
+    def final_matrix(self, csi_corr, csi_target):
+
+        csi_cr = self.csi(csi_corr)
+        csi_tr = self.csi(csi_target)
+        rl_dod = np.zeros(3)
+        rl_doa = np.zeros(3)
+
+        for i in range(0, 3):
+            rl_dod[i] = np.angle(csi_cr[0, i, 0, 0] * np.conjugate(csi_cr[1, i, 0, 0]))
+        for i in range(1, 3):
+            rl_doa[i] = np.angle(csi_cr[0, 0, 0, 0] * np.conjugate(csi_cr[0, i, 0, 0]))
+        csi_tro = np.zeros((2, 3, 1, np.size(csi_tr, 3)), dtype=complex)
+        for k in range(0, np.size(csi_tr, 3) - 1):
+
+            csi_tro[0, :, 0, k] = csi_tr[0, :, 0, k]
+
+            for i in range(0, 3):
+                csi_tro[1, i, 0, k] = csi_tr[1, i, 0, k] * np.exp(1j * rl_dod[i])
+
+            for i in range(1, 3):
+                csi_tro[:, i, 0, k] = csi_tro[:, i, 0, k] * np.exp(1j * rl_doa[i])
+        return csi_tro
 
     def csi(self,csi):
 
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from Bplate import *
-csip=csi_matrix()
-c=csip.csi('file1.dat')
+c=csi_matrix('file1.dat', 'file1.dat')
+# c=csip.final_matrix('file1.dat', 'file1.dat')
 print(c[:,:,0,0])
